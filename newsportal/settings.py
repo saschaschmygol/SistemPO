a9fe412e349366118a0dc22b113a8f07dac7be6c
Александр Шмыголь <113779803+saschaschmygol@users.noreply.github.com>
2023-12-12 21:58:03 +0500

    settings

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4p9u6*_+76s6b01*y_8!x2l3$_ppblvp3vt#^06jyvykwn%09b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saschaschmygol.onrender.com']
 
 
 # Application definition
