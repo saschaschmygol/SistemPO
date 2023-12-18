728679f47fae35736916678e578452d299b2a194
Александр Шмыголь <113779803+saschaschmygol@users.noreply.github.com>
2023-12-18 15:07:30 +0500

    +
    +

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4p9u6*_+76s6b01*y_8!x2l3$_ppblvp3vt#^06jyvykwn%09b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['saschaschmygol.onrender.com']
 
